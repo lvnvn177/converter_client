6c1ad710790057f5fa5928a40431feefd9d086fa
lvnvn177 <53424393+lvnvn177@users.noreply.github.com>
2024-11-27 21:40:45 +0900

    init

--- Code/converter/next.config.js
+++ Code/converter/next.config.js
@@ -7,10 +7,11 @@ const nextConfig = {
         // localhost:9000의 api 요청할 때 /api/~~ 로작성하면 'http://localhost:9000/~~' 로 요청한 것과 동일하게 적용이 된다.
         source: "/api/:path*",
         // cors로 문제가 되었던 url 입력
-        destination: "http://localhost:2000/:path*"
+        destination: process.env.NEXT_PUBLIC_API_SERVER + "/:path*" // 환경변수 사용
       }
     ]
   }
 } 
 
 module.exports = nextConfig
+
